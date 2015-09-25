54956034045fe2c17df68b25f7b9b70f44cfef3a
Darwin <prasetiodarwin@gmail.com>
2015-09-25 16:34:41 +0700

    attribute selection

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 /**
  * Created by nim_13512065 on 9/24/15.
  */
-import weka.*;
+import 
 public class Main {
     public static void main(String[] args) {
+        weka.classifiers.Classifier classifier = new Classifier() {
+
+            @Override
+            public void buildClassifier(Instances i) throws Exception {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
         System.out.println("SADF");
     }
 }
