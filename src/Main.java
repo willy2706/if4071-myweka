5966a2096c8ee1ca2d95b963a252d7ca5b8cd931
willy2706 <willylie2706@gmail.com>
2015-09-25 15:34:39 +0700

    add how toload arff file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 /**
  * Created by nim_13512065 on 9/24/15.
  */
-import weka.*;
+
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("SADF");
+    public final static String TRAINDATASETJ48 = "<isisendiri>.arff";
+    public final static String TESTDATASETJ48 = "<isisendiri>.arff";
+    public static void main(String[] args) throws Exception {
+        DataSource datasource = new DataSource(TRAINDATASETJ48);
+        Instances trainInstances = datasource.getDataSet();
+        trainInstances.setClassIndex(trainInstances.numAttributes()-1);
+        Evaluation evaluation = new Evaluation(trainInstances);
+
+        MyJ48 myJ48 = new MyJ48();
+
+        datasource = new DataSource(TESTDATASETJ48);
+        Instances testInstances = null;
+
+        evaluation.evaluateModel(myJ48,testInstances);
     }
 }
---
+++ src/MyJ48.java
@@ -0,0 +1,16 @@
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+
+/**
+ * Created by nim_13512065 on 9/25/15.
+ */
+public class MyJ48 extends J48 {
+    public MyJ48() {
+
+    }
+
+    @Override
+    public void buildClassifier (Instances instances) {
+
+    }
+}
