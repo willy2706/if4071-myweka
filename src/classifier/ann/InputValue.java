1f53a903bc329ab4b28d2cd45c833d45f8d9bef0
darwin-prasetio <prasetiodarwin@gmail.com>
2015-11-12 21:20:32 +0700

    changed input value attribute type from string to double

--- src/classifier/ann/InputValue.java
+++ src/classifier/ann/InputValue.java
@@ -4,6 +4,6 @@ package classifier.ann;
  * Created by nim_13512065 on 11/12/15.
  */
 public class InputValue {
-    private String value;
-    private String weight;
+    private double value;
+    private double weight;
 }
