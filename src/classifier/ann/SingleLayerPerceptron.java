3ea7189ec241a5db1ae7ffe3e91ff6aed8c2d235
darwin-prasetio <prasetiodarwin@gmail.com>
2015-11-12 18:40:46 +0700

    added input list and weight list

--- src/classifier/ann/SingleLayerPerceptron.java
+++ src/classifier/ann/SingleLayerPerceptron.java
@@ -1,7 +1,28 @@
 package classifier.ann;
 
+import java.util.List;
+
 /**
  * Created by nim_13512065 on 11/11/15.
  */
 public abstract class SingleLayerPerceptron extends MyANN {
+    List<Double> input;
+    List<Double> weight;
+
+    //enum activation function??
+    public List<Double> getInput() {
+        return input;
+    }
+
+    public void setInput(List<Double> input) {
+        this.input = input;
+    }
+
+    public List<Double> getWeight() {
+        return weight;
+    }
+
+    public void setWeight(List<Double> weight) {
+        this.weight = weight;
+    }
 }
