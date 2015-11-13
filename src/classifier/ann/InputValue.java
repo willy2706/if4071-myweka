aefebdc20be61dfadf11038b93e3090984c299fc
darwin-prasetio <prasetiodarwin@gmail.com>
2015-11-13 16:30:15 +0700

    ganti struktur data, tapi gak tau makenya gimana

--- src/classifier/ann/InputValue.java
+++ src/classifier/ann/InputValue.java
@@ -1,26 +1,28 @@
 package classifier.ann;
 
+import java.util.List;
+
 /**
  * Created by nim_13512065 on 11/12/15.
  */
 public class InputValue {
-    private double value;
-    private double weight;
+    private List<double[]> input;
+    private List<Double> weight;
 
-    public double getValue() {
-        return value;
+    public List<double[]> getInput() {
+        return input;
     }
 
-    public void setValue(double value) {
-        this.value = value;
+    public void addInput(List<double[]> value) {
+        this.input.addAll(value);
     }
 
-    public double getWeight() {
+    public List<Double> getWeight() {
         return weight;
     }
 
-    public void setWeight(double weight) {
-        this.weight = weight;
+    public void addWeight(List<Double> weight) {
+        this.weight.addAll(weight);
     }
     
     
