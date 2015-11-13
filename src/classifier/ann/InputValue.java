8bed145a4218369e04e076c4c836a4b851555b4d
darwin-prasetio <prasetiodarwin@gmail.com>
2015-11-13 11:43:45 +0700

    added getter and setter to input value

--- src/classifier/ann/InputValue.java
+++ src/classifier/ann/InputValue.java
@@ -6,4 +6,22 @@ package classifier.ann;
 public class InputValue {
     private double value;
     private double weight;
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
+    
 }
