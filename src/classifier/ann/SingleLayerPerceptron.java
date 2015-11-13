6ce4ff5c2965264a96929430e306ef645d3a07aa
darwin-prasetio <prasetiodarwin@gmail.com>
2015-11-13 18:14:56 +0700

    Merge origin/master
    Conflicts:
    	src/classifier/ann/SingleLayerPerceptron.java

--- src/classifier/ann/SingleLayerPerceptron.java
+++ src/classifier/ann/SingleLayerPerceptron.java
@@ -8,22 +8,16 @@ import java.util.List;
 /**
  * Created by nim_13512065 on 11/11/15.
  */
-public class SingleLayerPerceptron {
-    protected List<InputValue> weight;
+public class SingleLayerPerceptron{
     protected double learningRate;
     protected int maxIterate;
     protected double deltaMSE;
     protected double momentum;
     protected String initialWeight; //random or given
 
-
     public SingleLayerPerceptron() {
         
-    }
-
-    public List<InputValue> getWeight() {
-        return weight;
-    }
+    };
 
     public double getLearningRate() {
         return learningRate;
@@ -55,5 +49,13 @@ public class SingleLayerPerceptron {
 
     public void setMomentum(double momentum) {
         this.momentum = momentum;
+    }
+    
+    public String getInitialWeight() {
+        return initialWeight;
+}
+
+    public void setInitialWeight(String initialWeight) {
+        this.initialWeight = initialWeight;
     }
 }
